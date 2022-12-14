196ee6096c815f8c3cac6f7206a20d38771a114e
Raquel Revelles <sadierev@hotmail.com>
2022-12-14 20:47:18 +0000

    commit

---
+++ src/test/java/com/codeclan/example/WhiskyTracker/WhiskyTrackerApplicationTests.java
@@ -0,0 +1,40 @@
+package com.codeclan.example.WhiskyTracker;
+
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhiskyTrackerApplicationTests {
+
+	@Autowired
+	WhiskyRepository whiskyRepository;
+
+	@Test
+	public void contextLoads() {
+	}
+
+	@Test
+	public void canFindWhiskyByYear() {
+		List<Whisky> found = whiskyRepository.findByYear(2018);
+		assertEquals(6, found.size());
+
+	}
+
+	@Test
+	public void canFindWhiskyByYearAndAge() {
+		List <Whisky> foundWhiskies = whiskyRepository.findByDistilleryNameAndAge("Glendronach", 15);
+		assertEquals(1, foundWhiskies.size());
+
+	}
+
+}
